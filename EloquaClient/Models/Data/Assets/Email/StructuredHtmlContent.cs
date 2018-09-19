9d0f35e74bcda721e9203d7a83d7a1cdece52ffb
Unknown <shawn_johnson14@hotmail.com>
2018-09-19 10:35:48 -0600

    Add new models

---
+++ EloquaClient/Models/Data/Assets/Email/StructuredHtmlContent.cs
@@ -0,0 +1,13 @@
+﻿namespace LG.Eloqua.Api.Rest.ClientLibrary.Models.Data.Assets.Email
+{
+    public class StructuredHtmlContent
+    {
+        public string CssHeader { get; set; }
+        public string DocType { get; set; }
+        public string HtmlBody { get; set; }
+        public int? Id { get; set; }
+        public string JavascriptHeader { get; set; }
+        public string Root { get; set; }
+        public string Type => "StructuredHtmlContent";
+    }
+}
